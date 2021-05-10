4b2fd49fff9cd22bee60af4b66445da1b9d1aa46
John Florez <john.florez25@gmail.com>
2021-05-10 10:08:40 -0500

    Set up adoption app

---
+++ src/containers/AdoptionHome.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Box, Center, Text } from "@chakra-ui/react";
+
+export default class AdoptionHome extends Component {
+  render() {
+    return (
+      <Center>
+        <Box mt={400} fontSize="6xl" p={4} color="white">
+          <Text>Mi Página de Adopción</Text>
+        </Box>
+      </Center>
+    );
+  }
+}
